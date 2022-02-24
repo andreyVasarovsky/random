cf19288dc8c711157a8e879ee90963d3e8d3161d
Andrej Vasarovskij <andrej.vasarovskij@gmail.com>
2022-02-24 10:51:20 +0200

    First random task

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Tags;
+use App\Models\Categories;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/json', function (){
+//    $tags = DB::select('select * from tags'); //Return array of objects
+//    $categories = DB::select('select * from categories'); //Return array of objects
+    $tags = DB::connection()->getPdo()->query("select * from tags")->fetchAll(\PDO::FETCH_ASSOC); //Return array of arrays
+    $categories = DB::connection()->getPdo()->query("select * from categories")->fetchAll(\PDO::FETCH_ASSOC); //Return array of arrays
+
+    $data = [
+        'tags' => [],
+        'categories' => [],
+    ];
+    if (!empty($tags)){
+        foreach ($tags AS $tag){
+            $data['tags'][] = ['id' => $tag['id'], 'title' => $tag['title']];
+        }
+    }
+    if (!empty($categories)){
+        foreach ($categories AS $category){
+            $data['categories'][] = ['id' => $category['id'], 'title' => $category['title']];
+        }
+    }
+
+    return [
+        'data' => $data
+    ];
+
+    //Best solution:
+//    return [
+//        'data' => [
+//            'tags' => Tags::select('id', 'title')->get(),
+//            'categories' => Categories::select('id', 'title')->get(),
+//        ]
+//    ];
 });
